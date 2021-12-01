c7b881c0bf5b591dda63728c6a4d683d6d7b849d
nicholaswzk <nicholaswzk.nw@hotmail.com>
2021-12-01 08:07:44 -0800

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ class TestHello(unittest.TestCase):
     def test_hello(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
+        self.assertEqual(rv.data, b'Hello World!')
 if __name__ == '__main__':
     unittest.main()
